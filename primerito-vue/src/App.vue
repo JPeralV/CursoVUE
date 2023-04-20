534685674283825938eeac025a972fa702a7fdd7
Jose Peral <joseperalvazquez@gmail.com>
2023-04-20 13:35:48 +0200

    Instalacion vue y primeros pasos

---
+++ primerito-vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <h1>Heyyyyyy {{ name }}</h1>
+
+</template>
+
+<script setup>
+
+const name = 'Vue dinámico'
+
+</script>
+
+<style>
+
+h1{
+  color: aquamarine;
+  font-weight: bold;
+}
+
+</style>
