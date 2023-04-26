42b0706d0d04a64cc58a7cddabd453e87f0b8378
Jose Peral <joseperalvazquez@gmail.com>
2023-04-26 08:26:08 +0200

    Componentes reacivos

---
+++ primerito-vue/src/components/BlogPost.vue
@@ -0,0 +1,22 @@
+<script setup>
+//const props = defineProps (['title','id','body','colorText']);
+
+defineProps({
+  title: String,
+  id: Number,
+  body: {
+    type: String,
+    default : '"Sin descripcion",
+  }
+})
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5></h5>
+      <button @click="$emit('cambiarFavorito',"title")"></button>
+    </div>
+    <button>Counter</button>
+  </div>
+</template>
---
+++ primerito-vue/src/components/button.component.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div>
+    <button>Counter</button>
+  </div>
+</template>
